dbbaa2f1ea47debbf44d6d01563d730ce6c4826b
Siti Nur Mu'arivah <sitinurmuarivah92@gmail.com>
2023-04-30 22:25:38 +0700

    add using isMoving

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -1,28 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Character : MonoBehaviour
 {
-    // Update is called once per frame
+    bool isMoving;
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (isMoving)
+            return;
+
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.position += Vector3.forward;
+            direction += Vector3.forward;
         }
-        else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            transform.position += Vector3.back;
+            direction += Vector3.back;
         }
-        else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right;
+            direction += Vector3.right;
         }
-        else if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left;
+            direction += Vector3.left;
         }
-        
+
+        if(direction == Vector3.zero)
+            return;
+
+        Move(direction);
+
+    }
+
+    public void Move(Vector3 direction)
+    {
+        isMoving = true;
+        var moveTween = transform.DOMove(transform.position + direction, 0.1f);
+        moveTween.onComplete = () => isMoving = false;
     }
 }
